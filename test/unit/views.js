43c6c931f1fcd3633ab01ab17769c26a6d96b463
Cedric Ziel <cedric@cedric-ziel.com>
2013-12-14 19:24:27 +0100

    [TASK] Setup basic rendering

---
+++ test/unit/views.js
@@ -0,0 +1,20 @@
+// Load modules
+
+var Lab = require('lab');
+var Hapi = require('hapi');
+
+// Test shortcuts
+
+var expect = Lab.expect;
+var before = Lab.before;
+var after = Lab.after;
+var describe = Lab.experiment;
+var it = Lab.test;
+
+// Declare internals
+
+var internals = {};
+
+describe('Views', function () {
+
+});
