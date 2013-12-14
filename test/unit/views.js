220e3c7f089de928909d15a7322346c6aaded4a8
Cedric Ziel <cedric@cedric-ziel.com>
2013-12-14 20:37:29 +0100

    [TASK] Wire views

--- test/unit/views.js
+++ test/unit/views.js
@@ -10,11 +10,34 @@ var before = Lab.before;
 var after = Lab.after;
 var describe = Lab.experiment;
 var it = Lab.test;
-
-// Declare internals
-
-var internals = {};
+var S = Hapi.types.String;
 
 describe('Views', function () {
 
+	var viewsPath = 'templates';
+
+	var options = {
+		views: {
+			engines: { 'html': 'handlebars' },
+			path: viewsPath
+		}
+	};
+
+	var server = new Hapi.Server(options);
+
+	describe('#index-routes', function () {
+
+		it('shows template when correct path is provided', function (done) {
+			server.route({ method: 'GET', path: '/', handler: require('../../lib/routes/index-route').index });
+			server.inject(
+					{
+						method: 'GET',
+						url: '/'
+					}, function (res) {
+
+						expect(res.result).to.contain('Willkommen');
+						done();
+					});
+		});
+	});
 });
